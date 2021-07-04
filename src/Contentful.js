aba6a49144af16d42aec8aef4c44f87ce6239b61
Hyperion1040 <mateusz.worek@gmail.com>
2021-07-04 09:03:44 +0200

    commit

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,10 +1,6 @@
 import { createClient } from 'contentful';
 
 export default createClient({
-    space: ncsrqpji7dcw,
-    accessToken: maM4gMQdL1vbCIeTYZ7utx5zNCQquwPWLxy6zaP8Odg,
-    space: 'ncsrqpji7dcw',
-    accessToken: 'maM4gMQdL1vbCIeTYZ7utx5zNCQquwPWLxy6zaP8Odg',
     space: process.env.REACT_APP_API_SPACE,
     accessToken: process.env.REACT_APP_ACCESS_TOKEN
 });
